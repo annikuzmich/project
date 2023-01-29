4e96f1e492ddd735d8025ee2583f2f7abcbe0a9e
Ann <example@gmail.com>
2023-01-29 23:26:11 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use sctrict";
+//alert('Hello');
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Do you have 18?","18");
+//console.log(answer + 5);
+//const answers = [];
+//answers[0] = prompt('What is your name?',' ');
+//answers[1] = prompt('How old are you?',' ');
+//answers[2] = prompt('What is your  last name?',' ');
+//console.log(typeof(answers));
+//console.log(typeof(null));
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+const user  = 'Ivan';
+alert(`Hello, ${user}`)
